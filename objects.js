6bfef70b4994ab6dc3ea3882eeef05434b3cc9a2
Charles Kim <cwkim3@gmail.com>
2018-07-07 23:05:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {artistName: "songTitle"};
 function updatePlaylist(playlist, artistName, songTitle){
-    return Object.assign({}, playlist, {'Slowdive':'Alison', 'My Bloody Valentine':'Sometimes'});
+    return Object.assign({}, playlist, {'Slowdive':'Alison', 'My Bloody Valentine':'Sometimes', 'Phil Ochs':'Here's to the State of Mississippi'});
 }

72e0ee53fe743d98c08966d469876365f31305b1
Charles Kim <cwkim3@gmail.com>
2018-07-07 22:39:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,5 @@ function updatePlaylist(playlist, artistName, songTitle){
     playlist["Slowdive"] = Alison;
     playlist["My Bloody Valentine"] = Sometimes;
     playlist["Phil Ochs"] = 'Here’s to the State of Mississippi'; 
+    return playlist;
 }

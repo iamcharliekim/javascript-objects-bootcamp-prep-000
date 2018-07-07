0590d54ba3e71ddef7c67c6a4229e3647974aeb0
Charles Kim <cwkim3@gmail.com>
2018-07-07 23:29:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
 var playlist = { artist: "song" };
 function updatePlaylist(playlist, artist, song){
-    playlist[artist]=song;
-    return playlist;
+   return Object.assign({}, playlist, { [artist]: song });
 }
